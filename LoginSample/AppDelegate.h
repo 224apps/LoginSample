01d3d6773b65fc850ee97f14f22b0af32b136ebc
Abdoulaye Diallo <shotone@icloud.com>
2014-12-31 01:44:19 -0500

    Initial Commit

---
+++ LoginSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginSample
+//
+//  Created by A's macAir on 12/31/14.
+//  Copyright (c) 2014 Abdoulaye Diallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginSample
+//
+//  Created by A's macAir on 12/31/14.
+//  Copyright (c) 2014 Abdoulaye Diallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
